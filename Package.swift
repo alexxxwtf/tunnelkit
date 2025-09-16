5096f3f91707b2acbeb10a360d4d9f49bc782c31
alexxxwtf <alexxx.engineering@gmail.com>
2025-09-17 04:00:02 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -39,9 +39,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         
-        .package(url: "https://github.com/ZuperHQ/SwiftyBeaver", branch: "master"),
+        // .package(url: "https://github.com/ZuperHQ/SwiftyBeaver", branch: "master"),
 
-        // .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
      //   .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "3.5.1"),
          .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "1.1.11700"),
 //        .package(name: "WireGuardKit", url: "https://git.zx2c4.com/wireguard-apple", .exact("1.0.15-26")),
