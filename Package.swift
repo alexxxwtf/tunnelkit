c833549f7a19c3b3898080f707e03dbae8a08105
alexxxwtf <alexxx.engineering@gmail.com>
2025-09-17 04:03:16 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -46,8 +46,8 @@ let package = Package(
          .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "1.1.11700"),
 //        .package(name: "WireGuardKit", url: "https://git.zx2c4.com/wireguard-apple", .exact("1.0.15-26")),
         
-        .package(url: "https://github.com/WireGuard/wireguard-apple", .exact("1.0.15-26")),
-//        .package(name: "WireGuardKit", url: "https://github.com/groupofstars/wireguard-apple", .exact("3.0.0"))
+        // .package(url: "https://github.com/WireGuard/wireguard-apple", .exact("1.0.15-26")),
+       .package(name: "WireGuardKit", url: "https://github.com/groupofstars/wireguard-apple", .exact("3.0.0"))
 //        .package(name: "WireGuardKit", url: "https://github.com/passepartoutvpn/wireguard-apple", .revision("73d9152fa0cb661db0348a1ac11dbbf998422a50"))
 //        .package(name: "WireGuardKit", path: "../wireguard-apple")
     ],
